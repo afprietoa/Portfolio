95081586d9d4fbe36c7fcf6abf96a55578915b09
Andres Prieto <afprietoa@unal.edu.co>
2023-08-25 11:02:59 -0500

    Updated resources

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,14 @@ import { Footer, Navbar, Sidebar } from '../components'
 import Hero from '../assets/images/heroImg.png'
 import Avatar from '../assets/images/hero-img-small.jpg'
 import About from '../assets/images/aboutImg.png'
-import Project1 from '../assets/images/project-1.jpg'
-import Project2 from '../assets/images/project-2.jpg'
-import Project3 from '../assets/images/project-3.jpg'
-import Project4 from '../assets/images/project-4.jpg'
+import Project1 from '../assets/images/project-1.png'
+import Project2 from '../assets/images/project-2.png'
+import Project3 from '../assets/images/project-3.png'
+import Project4 from '../assets/images/project-4.png'
+import Blog1 from '../assets/images/blog-1.jpg'
+import Blog2 from '../assets/images/blog-2.jpg'
+import Blog3 from '../assets/images/blog-3.jpg'
+import Blog4 from '../assets/images/blog-4.jpg'
 import Connect  from '../assets/videos/space.mp4'
 import { useNavigate } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Home'
@@ -111,33 +115,27 @@ const Home = () => {
                     <h4>web development</h4>
                     <div className="underline"></div>
                     <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                      Dignissimos est temporibus quia aliquam reiciendis, 
-                      ipsa aperiam veritatis omnis ratione. Voluptates.
+                    Development of websites from the ground up that mirror the objectives and needs, ensuring they are responsive and compatible across mobile devices. 
                     </p>
                   </article>
                   {/* end of services */}
                                      {/* services */}
                                      <article className="service">
                     <i className='fab fa-sketch service-icon'></i>
-                    <h4>web desgin</h4>
+                    <h4>UX/UI desgin</h4>
                     <div className="underline"></div>
                     <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                      Dignissimos est temporibus quia aliquam reiciendis, 
-                      ipsa aperiam veritatis omnis ratione. Voluptates.
+                    Design of modern and visually appealing user interfaces (UI) that capture the essence of the personal brand, optimizing the user experience (UX) for intuitive navigation
                     </p>
                   </article>
                   {/* end of services */}
                                      {/* services */}
                                      <article className="service">
                     <i className='fab fa-android service-icon'></i>
-                    <h4>App design</h4>
+                    <h4>Mobile Development</h4>
                     <div className="underline"></div>
                     <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                      Dignissimos est temporibus quia aliquam reiciendis, 
-                      ipsa aperiam veritatis omnis ratione. Voluptates.
+                    Development of mobile apps integrating external platforms and working under a data management methodology in order to meet user requirements.
                     </p>
                   </article>
                   {/* end of services */}
@@ -151,11 +149,7 @@ const Home = () => {
             <h2>projects</h2>
             <div className="underline"></div>
               <p className="projects-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Voluptatem nam itaque autem quaerat laboriosam voluptas esse 
-                temporibus fugiat deserunt voluptates architecto culpa necessitatibus 
-                similique nemo voluptatibus, nesciunt dolores aut repellat? Quisquam 
-                deleniti nostrum quos et molestiae sint blanditiis atque hic?
+              The developed projects aim to provide technological solutions to challenges presented in various contexts. The creation of these projects was achieved utilizing diverse technologies such as React, Angular, Spring Boot, Django, among others.
               </p>       
           </div>
         {/* end of section title */}
@@ -165,8 +159,8 @@ const Home = () => {
           <article className="project">
             <img src={Project1} alt="single project" className='project-img'/>
             <div className="project-info">
-              <h4>project title</h4>
-              <p>Client name</p>
+              <h4>WaBike</h4>
+              <p>GA</p>
             </div>
           </article>
           </a>
@@ -176,8 +170,8 @@ const Home = () => {
           <article className="project">
             <img src={Project2} alt="single project" className='project-img'/>
             <div className="project-info">
-              <h4>project title</h4>
-              <p>Client name</p>
+              <h4>Health App</h4>
+              <p>UNAL</p>
             </div>
           </article>
           </a>
@@ -187,8 +181,8 @@ const Home = () => {
           <article className="project">
             <img src={Project3} alt="single project" className='project-img'/>
             <div className="project-info">
-              <h4>project title</h4>
-              <p>Client name</p>
+              <h4>Hospital at Home</h4>
+              <p>UNAL</p>
             </div>
           </article>
           </a>
@@ -198,8 +192,8 @@ const Home = () => {
           <article className="project">
             <img src={Project4} alt="single project" className='project-img'/>
             <div className="project-info">
-              <h4>project title</h4>
-              <p>Client name</p>
+              <h4>Reading Essence</h4>
+              <p>UNAL</p>
             </div>
           </article>
           </a>
@@ -222,10 +216,7 @@ const Home = () => {
               </div>
               {/* end of section title */}
               <p className="video-text">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum necessitatibus labore, 
-                id doloribus distinctio ad illo, dignissimos nobis fugiat, est itaque laboriosam 
-                praesentium nisi iste corporis error voluptas repudiandae odit nam blanditiis quod 
-                provident nesciunt. Dolor exercitationem rem sint dicta?
+              Don't hesitate to get in touch with me if you have any inquiries or would like to discuss your project. I'm enthusiastic about your thoughts and eager to see how we can work together. Whether you're considering a potential collaboration, embarking on a creative venture, or simply want to say hi, I'm here, and I'm eager to engage.
               </p>
               <a href="" className="btn" onClick={() => {navigate("/contact")}}>
                 contact me
@@ -322,7 +313,7 @@ const Home = () => {
         <div className="section timeline">
                       {/* section title */}
                       <div className="section-title">
-                      <h2>timeline</h2>
+                      <h2>Education</h2>
                       <div className="underline">
                       </div>
                       </div>
@@ -330,10 +321,9 @@ const Home = () => {
                       <div className="section-center timeline-center">
                         {/* single timeline item */}
                         <article className="timeline-item">
-                          <h4>2020</h4>
-                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Neque beatae nemo quod natus asperiores quam libero illum? 
-                            Eos, voluptate perspiciatis.</p>
+                          <h3>Computer Systems Engineering</h3>
+                          <h4>National University of Colombia  agu,2016</h4>
+                          <p>Plan, analyze, design, implement, manage, and evaluate computer systems and services based on them.</p>
                             <span className="number">
                               1
                             </span>
@@ -341,10 +331,9 @@ const Home = () => {
                         {/* end of single timeline item */}
                         {/* single timeline item */}
                         <article className="timeline-item">
-                          <h4>2019</h4>
-                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Neque beatae nemo quod natus asperiores quam libero illum? 
-                            Eos, voluptate perspiciatis.</p>
+                          <h3>Intermediate Front-End Web Development</h3>
+                          <h4>Francisco José de Caldas District University feb,2023 - jul,2023</h4>
+                          <p>Creation of efficient, secure frontends, taking into account best practices to ensure their scalability, maintainability, portability, and usability.</p>
                             <span className="number">
                               2
                             </span>
@@ -352,10 +341,9 @@ const Home = () => {
                         {/* end of single timeline item */}
                         {/* single timeline item */}
                         <article className="timeline-item">
-                          <h4>2018</h4>
-                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Neque beatae nemo quod natus asperiores quam libero illum? 
-                            Eos, voluptate perspiciatis.</p>
+                        <h3>Back-end Web Development</h3>
+                          <h4>MAKAIA Bootcamp jan,2023 - apr,2023</h4>
+                          <p>Building REST APIs using relational or non-relational databases using Java with Spring Framework.</p>
                             <span className="number">
                               3
                             </span>
@@ -363,10 +351,9 @@ const Home = () => {
                         {/* end of single timeline item */}
                         {/* single timeline item */}
                         <article className="timeline-item">
-                          <h4>2017</h4>
-                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Neque beatae nemo quod natus asperiores quam libero illum? 
-                            Eos, voluptate perspiciatis.</p>
+                        <h3>Business Agility Training G3 - ONE</h3>
+                          <h4>Alura + ONE - Oracle Next Education jan,2023 - apr,2023</h4>
+                          <p>Business agility for quick adaptation to changes, delivering value to customers.</p>
                             <span className="number">
                               4
                             </span>
@@ -374,10 +361,9 @@ const Home = () => {
                         {/* end of single timeline item */}
                         {/* single timeline item */}
                         <article className="timeline-item">
-                          <h4>2016</h4>
-                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Neque beatae nemo quod natus asperiores quam libero illum? 
-                            Eos, voluptate perspiciatis.</p>
+                          <h3>Diploma in Programing Skills with Emphasis in Wep Applications</h3>
+                          <h4>National University of Colombia jan,2022 - dic, 2022</h4>
+                          <p>Construction of 3-layer web applications deployed in the cloud, developed within an agile team framework.</p>
                             <span className="number">
                               5
                             </span>
@@ -385,10 +371,9 @@ const Home = () => {
                         {/* end of single timeline item */}
                         {/* single timeline item */}
                         <article className="timeline-item">
-                          <h4>2015</h4>
-                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Neque beatae nemo quod natus asperiores quam libero illum? 
-                            Eos, voluptate perspiciatis.</p>
+                          <h3>Front-End Web Development</h3>
+                          <h4>Geek Academy Bootcamp oct,2021 - dic,2021</h4>
+                          <p>Web development with an emphasis on React, Redux, and other front-end technologies.</p>
                             <span className="number">
                               6
                             </span>
@@ -411,11 +396,9 @@ const Home = () => {
                 <div className="card">
                   {/* front of the card */}
                   <div className="card-side card-front">
-                    <img src={Project1} alt="project-1" />
-                    <h4>article</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                       Eos adipisci ab officiis ipsam numquam quidem aspernatur?
-                        Facere error libero eligendi!</p>
+                    <img src={Blog1} alt="blog-1" />
+                    <h4>Angular Material Theming</h4>
+                    <p>There are 4 pre-built themes that come with Angular Material: deeppurple-amber, indigo-pink, pink-bluegrey, purple-green</p>
                     <div className="card-footer">
                       <img src={Avatar} alt="avatar"/>
                       <p>7 min read</p>
@@ -431,11 +414,9 @@ const Home = () => {
                 <div className="card">
                   {/* front of the card */}
                   <div className="card-side card-front">
-                    <img src={Project2} alt="project-2" />
-                    <h4>article</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                       Eos adipisci ab officiis ipsam numquam quidem aspernatur?
-                        Facere error libero eligendi!</p>
+                    <img src={Blog2} alt="blog-2" />
+                    <h4>Spring Security - JWT</h4>
+                    <p>Spring Security + JWT Authentication define a compact and self-contained way for securely transmitting information. </p>
                     <div className="card-footer">
                       <img src={Avatar} alt="avatar"/>
                       <p>7 min read</p>
@@ -451,11 +432,9 @@ const Home = () => {
                 <div className="card">
                   {/* front of the card */}
                   <div className="card-side card-front">
-                    <img src={Project3} alt="project-3" />
-                    <h4>article</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                       Eos adipisci ab officiis ipsam numquam quidem aspernatur?
-                        Facere error libero eligendi!</p>
+                    <img src={Blog3} alt="Blog-3" />
+                    <h4>React Component Patterns</h4>
+                    <p>The trade-offs of the different React patterns allow for more useful and reusable code by adhering to design principles.</p>
                     <div className="card-footer">
                       <img src={Avatar} alt="avatar"/>
                       <p>7 min read</p>
@@ -471,11 +450,10 @@ const Home = () => {
                 <div className="card">
                   {/* front of the card */}
                   <div className="card-side card-front">
-                    <img src={Project4} alt="project-4" />
-                    <h4>article</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                       Eos adipisci ab officiis ipsam numquam quidem aspernatur?
-                        Facere error libero eligendi!</p>
+                    <img src={Blog4} alt="blog-4" />
+                    <h4>Django RESTful APIs using Dj-Rest-Auth</h4>
+                    <p>Authentication with dj-rest-auth in a Django REST framework API for Sign-up, Sign in and Sign out, password change and password reset features. 
+</p>
                     <div className="card-footer">
                       <img src={Avatar} alt="avatar"/>
                       <p>7 min read</p>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/Projects'
-import Project1 from '../assets/images/project-1.jpg'
-import Project2 from '../assets/images/project-2.jpg'
-import Project3 from '../assets/images/project-3.jpg'
-import Project4 from '../assets/images/project-4.jpg'
+import Project1 from '../assets/images/project-1.png'
+import Project2 from '../assets/images/project-2.png'
+import Project3 from '../assets/images/project-3.png'
+import Project4 from '../assets/images/project-4.png'
 import { Footer, Navbar, Sidebar } from '../components'
 const Projects = () => {
   return (
@@ -30,15 +30,13 @@ const Projects = () => {
         <div className="project-container">
           <img src={Project1} alt="project-1" />
           <a href="" className="project-icon">
-            <i className="fas fa-home"></i>
+          <i class="fas fa-bicycle"></i>
           </a>
         </div>
         <div className="project-details">
-          <h4>project name</h4>
+          <h4>WaBike</h4>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-             Molestiae asperiores suscipit culpa iste, sint neque. 
-             Temporibus eveniet earum itaque nisi.
+          A web application that allows you to generate the shortest route, the app is integrated with EnCicla stations. It also enables referencing high accident zones and making real-time theft reports.
           </p>
           <div className="project-footer">
             <span>
@@ -52,17 +50,15 @@ const Projects = () => {
       {/* single project */}
       <article className="single-project">
         <div className="project-container">
-          <img src={Project1} alt="project-1" />
+          <img src={Project2} alt="project-2" />
           <a href="" className="project-icon">
-            <i className="fas fa-home"></i>
+          <i class="fas fa-heartbeat"></i>
           </a>
         </div>
         <div className="project-details">
-          <h4>project name</h4>
+          <h4>Health App</h4>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-             Molestiae asperiores suscipit culpa iste, sint neque. 
-             Temporibus eveniet earum itaque nisi.
+          The mobile application enables the visualization of specialties, as well as the search for medical personnel by specialty or by the doctor's name. It also provides hospital location services and user data visualization.
           </p>
           <div className="project-footer">
             <span>
@@ -76,17 +72,37 @@ const Projects = () => {
       {/* single project */}
       <article className="single-project">
         <div className="project-container">
-          <img src={Project1} alt="project-1" />
+          <img src={Project3} alt="project-3" />
           <a href="" className="project-icon">
-            <i className="fas fa-home"></i>
+          <i class="fas fa-medkit"></i>
           </a>
         </div>
         <div className="project-details">
-          <h4>project name</h4>
+          <h4>Hospital at Home</h4>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-             Molestiae asperiores suscipit culpa iste, sint neque. 
-             Temporibus eveniet earum itaque nisi.
+          The mobile application allows the visualization of specialties, as well as searching for medical staff by specialty or doctor's name. It also includes hospital location services and user data display.
+          </p>
+          <div className="project-footer">
+            <span>
+              <i className="fab fa-github"></i>
+            </span>
+            <a href="">source code</a>
+          </div>
+        </div>
+      </article>
+      {/* end of single project */}
+      {/* single project */}
+      <article className="single-project">
+        <div className="project-container">
+          <img src={Project4} alt="project-4" />
+          <a href="" className="project-icon">
+          <i class="fas fa-book-open"></i>
+          </a>
+        </div>
+        <div className="project-details">
+          <h4>Reading Essence</h4>
+          <p>
+          The system allows for generating searches, checkouts, and returns of bibliographic materials. It also provides book recommendations based on the reader's preferences.
           </p>
           <div className="project-footer">
             <span>
